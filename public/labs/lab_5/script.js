f361efafe0fde3572d151c7869fd09ac5a494501
Gabrielle Fagbohunka <gabbyf99@terpmail.umd.edu>
2021-03-10 19:14:42 -0500

    Lab 5 HTML and JS update

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,16 +1,51 @@
-function mapInit() {
-  // follow the Leaflet Getting Started tutorial here
-  return map;
-}
+// function mapInit() {
+//   // follow the Leaflet Getting Started tutorial here
+//   return map;
+// }
 
-async function dataHandler(mapObjectFromFunction) {
-  // use your assignment 1 data handling code here
-  // and target mapObjectFromFunction to attach markers
-}
+// async function dataHandler(mapObjectFromFunction) {
+//   // use your assignment 1 data handling code here
+//   // and target mapObjectFromFunction to attach markers
+// https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=YOUR_MAPBOX_ACCESS_TOKEN
+// }
+
+
+// window.onload = windowActions;
 
 async function windowActions() {
   const map = mapInit();
   await dataHandler(map);
-}
+  console.log('window loaded');
+  const form = document.querySelector('.userform');
+  const search = document.querySelector('#search');
+  const suggestions = document.querySelector('.suggestions');
+
+  const request = await fetch('/api')
+  const data = await request.json();
+
+  search.addEventListener('input', (event) => {
+      console.log('input', event.target.value)
+      const display = data.filter((record) => {
+          return record.city.toUpperCase().includes(event.target.value.toUpperCase()) || record.zip.includes(event.target.value);
+      });
+
+      display.forEach(restaurant => {
+          const newItem = document.createElement('li');
+          newItem.classList.add('list-item');
+          newItem.innerHTML = `
+          <h2> ${restaurant.name} </h2>
+          <address>
+              ${restaurant.category}
+              ${restaurant.address_line_1}
+              ${restaurant.city}
+              ${restaurant.zip}
+          </address>
+          `;
+          
+          suggestions.append(newItem);
+      });
+  })
+};
 
 window.onload = windowActions;
+
